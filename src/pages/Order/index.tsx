b0c7d7890ba47edd4a57e2a9a6cf5fcb91e8fe64
audisiofilho <obuzua46@gmail.com>
2022-07-25 15:07:52 -0300

    Buscando categorias

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -20,11 +20,32 @@ type RouteDetailParams = {
   };
 };
 
+type CategoryProps = {
+  id: string;
+  name: string;
+};
+
 type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
 
 export default function Order() {
   const route = useRoute<OrderRouteProps>();
   const navigation = useNavigation();
+
+  const [category, setCategory] = useState<CategoryProps[] | []>([]);
+  const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+
+  const [amount, setAmount] = useState("1");
+
+  useEffect(() => {
+    async function loadInfo() {
+      const response = await api.get("/category");
+
+      setCategory(response.data);
+      setCategorySelected(response.data[0]);
+    }
+
+    loadInfo();
+  }, []);
 
   async function handleCloseOrder() {
     try {
@@ -49,9 +70,11 @@ export default function Order() {
         </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.input}>
-        <Text style={{ color: "#fff" }}>Pizzas</Text>
-      </TouchableOpacity>
+      {category.length !== 0 && (
+        <TouchableOpacity style={styles.input}>
+          <Text style={{ color: "#fff" }}>{categorySelected?.name}</Text>
+        </TouchableOpacity>
+      )}
       <TouchableOpacity style={styles.input}>
         <Text style={{ color: "#fff" }}>Pizza de calabresa</Text>
       </TouchableOpacity>
@@ -60,7 +83,8 @@ export default function Order() {
         <Text style={styles.qtdText}>Quantidade</Text>
         <TextInput
           style={[styles.input, { width: "60%", textAlign: "center" }]}
-          value="1"
+          value={amount}
+          onChangeText={setAmount}
           placeholderTextColor="#f0f0f0"
           keyboardType="numeric"
         />
